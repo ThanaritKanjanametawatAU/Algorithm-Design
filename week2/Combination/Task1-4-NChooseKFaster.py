46e737daecd4aae3aa98a23539e346fa5b5025fe
Thanarit <u6410322@au.edu>
2023-06-29 10:14:59 +0700

    Correct Task1-4

--- week2/Combination/Task1-4-NChooseKFaster.py
+++ week2/Combination/Task1-4-NChooseKFaster.py
@@ -5,7 +5,6 @@ N = int(input())
 k = int(input())
 x = [None for _ in range(N)]
 
-
 def Combination(i, s):
     global x, N, k
     if s == k:
@@ -14,10 +13,12 @@ def Combination(i, s):
                 x[j] = 0
         print(*x)
         return 1
-    elif N-i+s == k:
-        for j in range(N):
-            if x[j] == None:
-                x[j] = 1
+    elif N-i == k-s:   # Change this line
+        for j in range(i, N):   # And also change to start from i
+            x[j] = 1
+        print(*x)
+        for j in range(i, N):   # reset values for next recursion
+            x[j] = None
         return 1
     elif N-i+s < k:
         return 0
@@ -26,9 +27,11 @@ def Combination(i, s):
         v = Combination(i+1, s)
         x[i] = 1
         v += Combination(i+1, s+1)
+        x[i] = None   # Add this line to reset value for next recursion
         return v
 
 st = time.process_time()
 print(Combination(0, 0))
 et = time.process_time()
 print(f"Running Time: {et-st}")
+
