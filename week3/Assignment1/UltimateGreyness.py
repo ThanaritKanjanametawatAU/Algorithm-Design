314298b005969c809fb8b62a51a01e1ad8c6571d
Thanarit <u6410322@au.edu>
2023-07-05 22:56:40 +0700

    Assignment1:Ultimate Greyness Done

--- week3/Assignment1/UltimateGreyness.py
+++ week3/Assignment1/UltimateGreyness.py
@@ -1,13 +1,35 @@
 import time
+import sys
+sys.setrecursionlimit(10000)
 N = int(input())
 Colors = []
 for _ in range(N):
     Colors.append(tuple(map(int, input().split())))
 
-def UltimateGreyness(N, Colors):
+x = [None for _ in range(N)]
+minDiff = sys.maxsize
+def Combination(i):
+    global x, N, minDiff
+    if N <= 0:
+        return
+    if i == N:
+        if sum(x) != 0:
+            vividness, dullness = 1, 0
+            for j in range(N):
+                if x[j]:
+                    vividness *= Colors[j][0]
+                    dullness += Colors[j][1]
+            minDiff = min(minDiff, abs(vividness-dullness))
 
-    return
+    else:
+        x[i] = 0
+        Combination(i+1)
+        x[i] = 1
+        Combination(i+1)
+
+
 st = time.process_time()
-print(UltimateGreyness(N, Colors))
+Combination(0)
+print(minDiff)
 et = time.process_time()
 print(f"Running Time: {et-st}")
