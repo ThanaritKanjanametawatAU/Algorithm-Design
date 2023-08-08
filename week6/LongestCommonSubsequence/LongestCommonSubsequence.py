b04f2f0c09f17ffaf8c7e321dea5f5dd816531c0
Thanarit <u6410322@au.edu>
2023-08-08 14:32:41 +0700

    Finished Week6: LIS

--- week6/LongestCommonSubsequence/LongestCommonSubsequence.py
+++ week6/LongestCommonSubsequence/LongestCommonSubsequence.py
@@ -1,20 +1,42 @@
-import time
+# Input
 N, M = map(int, input().split())
 Ns = list(map(int, input().split()))
 Ms = list(map(int, input().split()))
-LCS = [[None for _ in range(M+1)] for _ in range(N+1)]
-LCS[N] = [0 for _ in range(M+1)]
+
+# Memoization: Record
+LCS = [[None for _ in range(M + 1)] for _ in range(N + 1)]
+for i in range(N + 1):
+    LCS[i][M] = 0
+for j in range(M + 1):
+    LCS[N][j] = 0
+
+
 def LCSLength(i, j):
+    # Memoization: Recall
     if LCS[i][j] != None:
         return LCS[i][j]
 
-    if Ns[i] == Ms[j]:
-        LCS[i][j] = 1 + LCSLength(i+1, j+1)
-    else:
-        LCS[i][j] = max(LCSLength(i+1, j), LCSLength(i, j+1))
+    # Terminating
+    if i == N or j == M:
+        return 0
 
+    # Recursion
+    LongestLength = 0
+    if Ns[i] == Ms[j]:
+        LongestLength = 1 + LCSLength(i + 1, j + 1)
+    else:
+        LongestLength = max(LCSLength(i + 1, j), LCSLength(i, j + 1))
+
+
+    # Memoization: Record
+    LCS[i][j] = LongestLength
     return LCS[i][j]
-st = time.process_time()
-print(LCSLength(0,0))
-et = time.process_time()
-print(f"Running Time: {et-st}")
+
+# Dynamic Programming
+for i in range(N, -1, -1):
+    for j in range(M, -1, -1):
+        LCSLength(i, j)
+result = LCS[0][0]
+
+# Output
+print(result)
